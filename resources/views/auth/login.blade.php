eed83fa9ff7c506fcf4fa10760baa43e7a18442b
Rosandymaulana <rosandy1118@gmail.com>
2022-06-23 23:23:45 +0700

    Menambah Login dan route

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="{{ asset('style/assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('style/assets/css/login.css') }}">
+</head>
+<body>
+    <section class="h-100 gradient-form" style="background-color: #eee;">
+      <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-xl-10">
+            <div class="card rounded-3 text-black">
+              <div class="row g-0">
+                <div class="col-lg-6">
+                  <div class="card-body p-md-5 mx-md-4">
+                    <div class="text-center">
+                      <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
+                        style="width: 185px;" alt="logo">
+                      <h4 class="mt-1 mb-5 pb-1">Koperasi Simpan Pinjam</h4>
+                    </div>
+<<<<<<< HEAD
+
+                    <form>
+                      <center><p>Masukkan Username dan Password</p></center>
+
+=======
+    
+                    <form>
+                      <center><p>Masukkan Username dan Password</p></center>
+    
+>>>>>>> 1ca448af9ad9e78359b01f89e2c8aa43e8fca998
+                      <div class="form-outline mb-4">
+                        <label class="form-label" for="form2Example11">Username</label>
+                        <input type="email" id="form2Example11" class="form-control" placeholder="Enter Username" />
+                      </div>
+<<<<<<< HEAD
+
+=======
+    
+>>>>>>> 1ca448af9ad9e78359b01f89e2c8aa43e8fca998
+                      <div class="form-outline mb-4">
+                        <label class="form-label" for="form2Example22">Password</label>
+                        <input type="password" id="form2Example22" class="form-control" placeholder="Enter Password"/>
+                      </div>
+<<<<<<< HEAD
+
+                      <div class="text-center pt-1 mb-5 pb-1">
+                        <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3 mt-3" type="button">Login</button>
+                      </div>
+
+=======
+    
+                      <div class="text-center pt-1 mb-5 pb-1">
+                        <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3 mt-3" type="button">Login</button>
+                      </div>
+    
+>>>>>>> 1ca448af9ad9e78359b01f89e2c8aa43e8fca998
+                      <div class="d-flex align-items-center justify-content-center pb-4">
+                        <p class="mb-0 me-2">Belom Punya Akun</p>
+                        <button type="button" class="btn btn-outline-danger m-3">Buat Akun</button>
+                      </div>
+<<<<<<< HEAD
+
+                    </form>
+
+=======
+    
+                    </form>
+    
+>>>>>>> 1ca448af9ad9e78359b01f89e2c8aa43e8fca998
+                  </div>
+                </div>
+                <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+                  <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                    <h4 class="mb-4">Buka Simpanan Berjangka Semakin Mudah</h4>
+                    <p class="small mb-0 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                      exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+</body>
+<<<<<<< HEAD
+</html>
+=======
+</html>
+>>>>>>> 1ca448af9ad9e78359b01f89e2c8aa43e8fca998
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function ($id) {
+    return view('user.home');
+});
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::get('/login', function () {
+        return view('auth.login');
+    });
+
+    Route::get('/register', function () {
+        return view('auth.register');
+    });
+});
+
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/about', function () {
+        return view('user.about', [
+            "title" => "about"
+        ]);
+    });
+
+    Route::get('/apply', function () {
+        return view('user.services', [
+            "title" => "apply"
+        ]);
+    });
+
+    Route::get('/services', function () {
+        return view('user.services', [
+            "title" => "services"
+        ]);
+    });
+
+    Route::get('/contact', function () {
+        return view('user.contact', [
+            "title" => "contact"
+        ]);
+    });
 });
