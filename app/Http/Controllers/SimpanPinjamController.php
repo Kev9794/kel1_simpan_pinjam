324af4326cad95e52b2a4091094da50b6a9c6691
fifirofiqoh28 <rofiqohwahyuningtyas@gmail.com>
2022-06-25 13:15:55 +0700

    controllerauth

---
+++ app/Http/Controllers/SimpanPinjamController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SimpanPinjamController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $anggota = $anggota =DB::table('anggota')->get();
+        $posts = Anggota::orderBy('id_anggota')->paginate(6);
+        return view('anggota.index',compact('anggota'));
+        with('i',(request()->input('page',1)-1)*5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('auth.');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'id_anggota'=>'required',
+            'nama'      =>'required',
+            'email'     =>'required',
+            'password'  =>'required',
+            'no_telp'   =>'required',
+            'alamat'    =>'required',
+        ]);
+        Anggota::create($request->all());
+
+        return redirect()->route('anggota.index')
+        ->with('success','Anggota Berhasil Ditambahkan');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $anggota = Anggota::find($id);
+        return view('mahasiswa.detail',compact('Anggota'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $anggota = DB::table('anggota')->where('id',$id)->first();;
+        return view('anggota.edit')
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
