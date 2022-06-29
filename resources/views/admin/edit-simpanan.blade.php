014d7a9bc6cd0202d419fe6151db1a9ae62e183d
Kev9794 <k.natanael88@gmail.com>
2022-06-29 12:27:17 +0700

    update

--- resources/views/admin/edit-simpanan.blade.php
+++ resources/views/admin/edit-simpanan.blade.php
@@ -2,7 +2,7 @@
 
 @section('container')
 <form>
-    <h4 class="fw-bolder text-center">Edit Peminjaman</h4>
+    <h4 class="fw-bolder text-center">Edit Simpanan</h4>
     <div class="form-group">
         <label for="tgl_simpan" class="col-form-label">Tanggal Simpan:</label>
         <input type="date" class="form-control" id="tgl_simpan">
